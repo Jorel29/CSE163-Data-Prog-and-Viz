1c6c538a1d294641aa866d486a68f8b03d519c6d
Jorel Huerto <jjhue29@gmail.com>
2022-10-17 12:39:26 -0700

    comment blocks

--- Assignments/Assignment4/MultilineChart.js
+++ Assignments/Assignment4/MultilineChart.js
@@ -4,7 +4,7 @@
 
 /*
 Code below provided by SureshLodha from BarGraphSamplev5.js
-*/
+=====================================================================================*/
 var margin = {top: 10, right: 40, bottom: 150, left: 50},//defining the margins
 width = 760 - margin.left - margin.right, //setting width based on margins
 height = 500 - margin.top - margin.bottom;//setting height based on margins
@@ -14,7 +14,10 @@ height = 500 - margin.top - margin.bottom;//setting height based on margins
 // Add comments here in your own words to explain this segment of code
 // select the body and add a svg area that is 760 W x 500 H
 var svg = d3.select("body").append("svg")
-.attr("width", width + margin.left + margin.right) // 760 W
-.attr("height", height + margin.top + margin.bottom) // 500H
-.append("g") //add grouped elements to body
-.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("width", width + margin.left + margin.right) // 760 W
+    .attr("height", height + margin.top + margin.bottom) // 500H
+    .append("g") //add grouped elements to body
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//==================================================================================
+
