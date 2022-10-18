0ff43521fd8ce89ba9414adafa482a05df7939a0
Jorel Huerto <jjhue29@gmail.com>
2022-10-17 21:41:00 -0700

    inital implementation

--- Assignments/Assignment4/MultilineChart.js
+++ Assignments/Assignment4/MultilineChart.js
@@ -5,7 +5,7 @@
 /*
 Code below provided by SureshLodha from BarGraphSamplev5.js
 =====================================================================================*/
-var margin = {top: 10, right: 40, bottom: 150, left: 50},//defining the margins
+/*var margin = {top: 10, right: 40, bottom: 150, left: 50},//defining the margins
 width = 760 - margin.left - margin.right, //setting width based on margins
 height = 500 - margin.top - margin.bottom;//setting height based on margins
 
@@ -18,7 +18,15 @@ var svg = d3.select("body").append("svg")
     .attr("height", height + margin.top + margin.bottom) // 500H
     .append("g") //add grouped elements to body
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+*/
+var svgWidth = 960,
+    svgHeight = 500;
 
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = svg.attr("width") - margin.left - margin.right,
+    height = svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 //==================================================================================
 
 //parse the year (format full year) (from D3 API)
@@ -64,9 +72,43 @@ d3.csv("BRICSdata.csv", type).then(function(data){
     x.domain(d3.extent(data, function(d) { return d.year; }));
 
     y.domain([
-        d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.energy; }); }),
-        d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.energy; }); })
+        d3.min(countries, function(c) { return d3.min(c.values, function(d) { return d.energy; }); }),
+        d3.max(countries, function(c) { return d3.max(c.values, function(d) { return d.energy; }); })
     ]);
 
-    
+    z.domain(countries.map(function(c) { return c.id; }));
+  
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("fill", "#000")
+      .text("Energy Per Capita, $(Millions)");
+
+  var country = g.selectAll(".country")
+    .data(countries)
+    .enter().append("g")
+      .attr("class", "country");
+  
+  country.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.values); })
+      .style("stroke", function(d) { return z(d.id); });
+
+  country.append("text")
+      .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + x(d.value.year) + "," + y(d.value.energy) + ")"; })
+      .attr("x", 3)
+      .attr("dy", "0.35em")
+      .style("font", "10px sans-serif")
+      .text(function(d) { return d.id; });
 });
