6421a207370162ce9f1ee06d6c994c76f3dee85a
Jorel Huerto <jjhue29@gmail.com>
2022-11-21 15:56:07 -0800

    Color Swap works

--- Assignments/Assignment7/FLPopDensity.js
+++ Assignments/Assignment7/FLPopDensity.js
@@ -1,20 +1,20 @@
 //Define Margin
-var margin = {left: 80, right: 80, top: 50, bottom: 50 }, 
-width = 960 - margin.left -margin.right,
-height = 660 - margin.top - margin.bottom;
+var margin = { left: 80, right: 80, top: 50, bottom: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 660 - margin.top - margin.bottom;
 
 //Define Color
 var color = d3.scaleOrdinal(d3.schemeCategory10);
 
 //Define SVG
 var svg = d3.select("body")
-.append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
 
 var projection = d3.geoAlbersUsa()
     .scale(5000)
-    .translate([width /2 -900, height / 2 -800]);
+    .translate([width / 2 - 900, height / 2 - 800]);
 
 var path = d3.geoPath()
     .projection(projection);
@@ -26,11 +26,11 @@ var tooltip = d3.select("body")
     .style("opacity", 0)
     .attr("class", "tooltip")
 
-var mouseover = function(event, d) {
+var mouseover = function (event) {
     tooltip
         .style("opacity", 0.8)
-        .style("left", (d3.pointer(event)[0]) )
-        .style("bottom", (d3.pointer(event)[1]) )
+        .style("left", (d3.pointer(event)[0]))
+        .style("bottom", (d3.pointer(event)[1]))
     d3.select(this)
         .style("opacity", 0.8)
     //console.log(d3.pointer(event))
@@ -38,15 +38,15 @@ var mouseover = function(event, d) {
     //console.log(d3.pointer(event)[0])
     //console.log(d3.pointer(event)[1])
     //console.log(tooltip.style.top);
-    }
-
-var mouseout = function() {
-tooltip
-    .style("opacity", 0)
-d3.select(this)
-    .style("opacity", 1)
 }
-    
+
+var mouseout = function () {
+    tooltip
+        .style("opacity", 0)
+    d3.select(this)
+        .style("opacity", 1)
+}
+
 //=================================================
 //tooltip end
 //=================================================   
@@ -54,21 +54,38 @@ d3.select(this)
 //color button start
 //================================================= 
 var colorButton = svg.append("g")
-            .attr("class", "button")
-            .attr("value", "OFF")
-            .attr("transform", "translate(0,80)")
-           
+    .attr("class", "button")
+    .attr("value", "OFF")
+    .attr("transform", "translate(0,80)")
+
 colorButton
     .append("rect")
-    .attr("height","20")
+    .attr("height", "20")
     .attr("x", 850)
     .attr("width", "65")
-    .attr("rx","5")
+    .attr("rx", "5")
     .attr("fill", "lightgray")
     .attr("stroke", "black")
-    .on("mouseover", function(){d3.select(this).style("opacity", 0.8)})
-    .on("mouseout", function(){d3.select(this).style("opacity", 1)})
-    .on("click", setScheme());
+    .on("mouseover", function () { d3.select(this).style("opacity", 0.8) })
+    .on("mouseout", function () { d3.select(this).style("opacity", 1) })
+    .on("click", function(){
+        var newScheme = d3.scaleThreshold()
+                .domain([1, 10, 50, 200, 500, 1000, 2000, 4000])
+        if (colorButton.attr("value")=="OFF")
+        {
+            colorButton.attr("value", "ON")
+            newScheme = newScheme.range(d3.schemeOrRd[9])
+            legend.attr("fill", function(d){return newScheme(d[0])})
+            counties.attr("fill", function(d){return newScheme(d.properties.density)})
+        }
+        else
+        {
+            colorButton.attr("value", "OFF")
+            newScheme = newScheme.range(d3.schemePuBu[9])
+            legend.attr("fill", function(d){return newScheme(d[0])})
+            counties.attr("fill", function(d){return newScheme(d.properties.density)})
+        }
+    });
 
 colorButton
     .append("text")
@@ -83,22 +100,11 @@ var color = d3.scaleThreshold()
     .domain([1, 10, 50, 200, 500, 1000, 2000, 4000])
     .range(d3.schemeOrRd[9]);
 
-function setScheme(){
-    if(colorButton.attr("value")=="OFF"){
-        color = d3.scaleThreshold()
-            .domain([1, 10, 50, 200, 500, 1000, 2000, 4000])
-            .range(d3.schemeOrRd[9]);
-        console.log("OrRd");
-    }else{
-        color = d3.scaleThreshold()
-            .domain([1, 10, 50, 200, 500, 1000, 2000, 4000])
-            .range(d3.schemePuBu[9]);
-        console.log("puBU");
-    }
-}
-
 //=================================================
 //color button end
+//=================================================
+//=================================================
+// Built from code from CAPop Density
 //=================================================
 var x = d3.scaleSqrt()
     .domain([0, 4500])
@@ -107,19 +113,20 @@ var x = d3.scaleSqrt()
 var g = svg.append("g")
     .attr("class", "key")
     .attr("transform", "translate(0,40)");
-
-g.selectAll("rect")
-  .data(color.range().map(function(d) {
-      d = color.invertExtent(d);
-      if (d[0] == null) d[0] = x.domain()[0];
-      if (d[1] == null) d[1] = x.domain()[1];
-      return d;
+//data mapping to color range with catches for null values
+var legend = g.selectAll("rect")
+    .data(color.range().map(function (d) {
+        d = color.invertExtent(d);
+        if (d[0] == null) d[0] = x.domain()[0];
+        if (d[1] == null) d[1] = x.domain()[1];
+        return d;
     }))
-  .enter().append("rect")
+    .enter().append("rect")
     .attr("height", 8)
-    .attr("x", function(d) { return x(d[0]); })
-    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
-    .attr("fill", function(d) { return color(d[0]); });
+    .attr("x", function (d) { return x(d[0]); })
+    .attr("width", function (d) { return x(d[1]) - x(d[0]); })
+    .attr("fill", function (d) { return color(d[0]); });
+
 
 g.append("text")
     .attr("class", "caption")
@@ -133,77 +140,84 @@ g.append("text")
 g.call(d3.axisBottom(x)
     .tickSize(13)
     .tickValues(color.domain()))
-  .select(".domain")
+    .select(".domain")
     .remove();
 
-d3.csv("FLCountiesDensity.csv").then(function(countiesDensity){
-    d3.json("us-10m.json").then(function(topology) {
+//variables to make the svg graphics avaliable to the global scope
+var counties;
+var borders;
+d3.csv("FLCountiesDensity.csv").then(function (countiesDensity) {
+    d3.json("us-10m.json").then(function (topology) {
         //console.log(topology.objects)
         //console.log(topology.objects.counties)
         //console.log(topology.objects.counties.geometries[0].id)
-        var topoFLCounties = topology.objects.counties.geometries.filter(function(c){
-            return c.id > 12000 && c.id<13000 ;
+        var topoFLCounties = topology.objects.counties.geometries.filter(function (c) {
+            return c.id > 12000 && c.id < 13000;
         })
         topology.objects.counties.geometries = topoFLCounties
         //console.log(topoFLCounties)
         //console.log(topology.objects.counties)
-    //Appending density to topology data (IDV for Web)
-    for (var i = 0; i < countiesDensity.length; i++) {
-				
-        //Grab state name
-        var countiesDensityID = countiesDensity[i].id2;
-        var name = countiesDensity[i].name;
-        //Grab data value, and convert from string to float
-        var density = +countiesDensity[i].density;
-        //console.log(topoFLCounties.length)
-        //Find the corresponding state inside the GeoJSON
-        for (var j = 0; j < topoFLCounties.length; j++) {
-        
-            var topoState = topoFLCounties[j].id;
-            //console.log(topoState +" "+ countiesDensityID)
-            if (countiesDensityID == topoState) {
-                
-                //Copy the data value into the JSON
-                topology.objects.counties.geometries[j].properties = {density, name}
-        
-                break;
+        //Appending density to topology data (IDV for Web)
+        for (var i = 0; i < countiesDensity.length; i++) {
+
+            //Grab state name
+            var countiesDensityID = countiesDensity[i].id2;
+            var name = countiesDensity[i].name;
+            //Grab data value, and convert from string to float
+            var density = +countiesDensity[i].density;
+            //console.log(topoFLCounties.length)
+            //Find the corresponding state inside the GeoJSON
+            for (var j = 0; j < topoFLCounties.length; j++) {
+
+                var topoState = topoFLCounties[j].id;
+                //console.log(topoState +" "+ countiesDensityID)
+                if (countiesDensityID == topoState) {
+
+                    //Copy the data value into the JSON
+                    topology.objects.counties.geometries[j].properties = { density, name }
+
+                    break;
+                }
             }
-        }		
-    }
+        }
 
-    //console.log(topology.objects.counties.geometries[0])
+        //console.log(topology.objects.counties.geometries[0])
 
-    svg.append("g")
-        .attr("class", "density")
-        .selectAll("path")
-        .data(topojson.feature(topology, topology.objects.counties).features)
-        .enter().append("path")
-        .attr("fill", function(d) {
-            return color(d.properties.density); })
-        .attr("d", path)
-        .on("mouseover", mouseover)
-        .on("mousemove", function(event, d) {
-            tooltip
-                .html(
-                    "<div class=\"item\">"+
-                        d.properties.name+":"+ d.properties.density+
-                    "</div>"
-                    )
-                .style("left", (d3.pointer(event)[0]) + "px")
-                .style("top", (d3.pointer(event)[1]) +25+"px")
+        counties = svg.append("g")
+            .attr("class", "density")
+            .selectAll("path")
+            .data(topojson.feature(topology, topology.objects.counties).features)
+            .enter().append("path")
+            .attr("fill", function (d) {
+                return color(d.properties.density);
             })
-        .on("mouseout", mouseout);
+            .attr("d", path)
+            .on("mouseover", mouseover)
+            .on("mousemove", function (event, d) {
+                tooltip
+                    .html(
+                        "<div class=\"item\">" +
+                        d.properties.name + ":" + d.properties.density +
+                        "</div>"
+                    )
+                    .style("left", (d3.pointer(event)[0]) + "px")
+                    .style("top", (d3.pointer(event)[1]) + 25 + "px")
+            })
+            .on("mouseout", mouseout);
 
-    svg.append("path")
-        .attr("class", "counties")
-        .datum(topojson.feature(topology, topology.objects.counties))
-        .attr("fill", "gray")
-        .attr("stroke", "#000")
-        .attr("stroke-opacity", 1)
-        .attr("d", path);
-    
+        borders = svg.append("path")
+            .attr("class", "counties")
+            .datum(topojson.feature(topology, topology.objects.counties))
+            .attr("fill", "gray")
+            .attr("stroke", "#000")
+            .attr("stroke-opacity", 1)
+            .attr("d", path);
+
+        
     });
 });
+
+
 
 
 
